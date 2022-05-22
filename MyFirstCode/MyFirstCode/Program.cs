2bdaa4651892d7ddf412726918c39f8ff66d1aae
Kamran Qadir <qadir0108@hotmail.com>
2022-05-22 22:39:14 +0500

    My First Code and My Second Code

---
+++ MyFirstCode/MyFirstCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MySecondCode/MySecondCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int marks = 80;
+            Console.WriteLine(marks);
+
+            long balance = 435435435435435;
+            Console.WriteLine(balance);
+
+            double price = 56.67;
+            Console.WriteLine(price);
+
+            char first = 'A';
+            Console.WriteLine(first);
+
+            bool IsMale = true;
+            Console.WriteLine(IsMale);
+
+            String name = "Kamran Qadir";
+            Console.WriteLine(name);
+
+            Console.ReadKey();
+        }
+    }
+}
