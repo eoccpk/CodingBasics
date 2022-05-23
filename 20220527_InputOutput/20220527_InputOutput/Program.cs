2aaef52f2f990328daa0052412039a52c37a5204
Kamran Qadir <qadir0108@hotmail.com>
2022-05-23 22:20:46 +0500

    added input output examples

---
+++ 20220527_InputOutput/20220527_InputOutput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _20220527_InputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output
+            Console.WriteLine("UBL - Where you come first");
+
+            // Input
+            string CardNumber = Console.ReadLine();
+
+            // Output
+            Console.WriteLine("You entered Card Number: " + CardNumber);
+
+            // Input
+            int PINCode = int.Parse(Console.ReadLine());
+
+            // Output
+            Console.WriteLine("You entered PIN Code: " + PINCode);
+
+            /* 
+             * Processing Steps:
+             * 1. Match CardNumber and PINCode with already saved
+             * 2. If Matched, show next screen
+             * 3. If DOES NOT matched, show error
+            */ 
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 20220527_Output/20220527_Output/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220527_Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print Value
+            Console.WriteLine(80);
+            Console.WriteLine("Kamran Qadir");
+
+            // Print Variable
+            int marks;
+            marks = 80;
+            Console.WriteLine(marks);
+
+            marks = 50;
+            Console.WriteLine(marks);
+
+            // Print String and Variable
+            Console.WriteLine("Your marks = " + marks);
+
+            Console.ReadKey();
+        }
+    }
+}
