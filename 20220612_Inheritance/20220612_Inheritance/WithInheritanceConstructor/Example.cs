43a9efad4066bc02e93b7e7370f952e424109d78
Kamran Qadir <qadir0108@hotmail.com>
2022-06-25 23:49:14 +0500

    creating parent object

--- 20220612_Inheritance/20220612_Inheritance/WithInheritanceConstructor/Example.cs
+++ 20220612_Inheritance/20220612_Inheritance/WithInheritanceConstructor/Example.cs
@@ -9,9 +9,14 @@ namespace _20220612_Inheritance.WithInheritanceConstructor
             Console.WriteLine("=========================================");
             Console.WriteLine("=====Inheritance Constructor Call========");
             Console.WriteLine("=========================================");
+
+            Person person = new Person("CNIC-123", "Person Name");
+            person.Show();
+
             Student student = new Student("CNIC-123", "Student123", 123, 500);
             Console.WriteLine("Student Data");
             student.Show();
+
             Console.WriteLine("=========================================");
             Teacher teacher = new Teacher("CNIC-321", "Teacher321", 321, "Coding");
             Console.WriteLine("Teacher Data");
@@ -22,10 +27,11 @@ namespace _20220612_Inheritance.WithInheritanceConstructor
              * - set Parent class show function as *virtual*
              * - change Child class show function as *override*
             */
-            //Person student = new Student("CNIC-123", "Student123", 123, 500);
-            //student.Show();
-            //Person teacher = new Teacher("CNIC-321", "Teacher321", 321, "Coding");
-            //teacher.Show();
+            Person p1 = new Student("CNIC-123", "Student123", 123, 500);
+            p1.Show();
+
+            Person p2 = new Teacher("CNIC-321", "Teacher321", 321, "Coding");
+            p2.Show();
         }
     }
 }
