f05b869dd7e3a4a87d2400698cb4474f1296f246
Kamran Qadir <qadir0108@hotmail.com>
2022-07-16 18:14:53 +0500

    added example desktop app

---
+++ 20220708_DesktopApp/20220708_DesktopApp/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _20220708_DesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "kamran" && txtPassword.Text == "12345")
+            {
+                lblMessage.Text = "Welcome " + txtUsername.Text;
+                MessageBox.Show("Logged-In Successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Wrong User Name or Password.");
+            }
+        }
+    }
+}
